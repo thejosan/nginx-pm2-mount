e008c5ce7776691e097c0b19e08988411045d09c
thejosan <33949813+thejosan@users.noreply.github.com>
2018-08-02 16:43:32 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sed -i "s/yourproxyurl/$PROXYURL/g" /etc/nginx/conf.d/default.conf
-cd /app/project && pm2 start npm --name app -- run sdt:start
+cd $APPDIR && pm2 start npm --name app -- run sdt:start
 /usr/sbin/nginx -g "daemon off;"
